a951227349cb0e2b7f27df18c1a6c75af93a141d
fargelus <ddraudred@gmail.com>
2020-02-05 10:28:55 +0300

    Setup project

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/mc_scheduler'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/mc_scheduler_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/mc_scheduler_development'
+    #    adapter :sql, 'mysql://localhost/mc_scheduler_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/mc_scheduler/mailers'
+
+    # See https://guides.hanamirb.org/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: https://guides.hanamirb.org/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+end
